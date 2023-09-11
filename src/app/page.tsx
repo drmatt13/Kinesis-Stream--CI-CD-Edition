ff103eb2eae0d41ff1ae1b3d2def6366f694bdb7
Matthew Sweeney <Matthew.Sweeney001@gmail.com>
2023-09-11 13:43:24 -0400

    9/11/23

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,93 @@
-import Image from 'next/image'
+"use client";
+
+/* eslint-disable @next/next/no-img-element */
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="w-full flex flex-col items-center mt-20">
+        <h1 className="text-2xl font-bold mb-8">
+          Kinesis Stream: CI/CD Edition
+        </h1>
+
+        <video
+          src="/kinesis.mp4"
+          controls={true}
+          className="max-w-full w-[48rem]"
+        ></video>
+        <a
+          href="https://github.com/drmatt13/ci-cd-kinesis-react-demo"
+          className="text-blue-600 underline cursor-pointer mt-8 text-2xl hover:text-blue-700 visited:text-purple-600"
+        >
+          github repo link
+        </a>
+        <div className="mt-12 max-w-[90vw]">
+          <p>
+            This project serves a multifaceted purpose: to demonstrate advanced
+            CI/CD capabilities, expertise in Kinesis data streaming, and
+            proficiency in front-end design using React. Within the visual
+            showcase, React features five clickable data streaming functions
+            which are showcased on the left. Though they share the same
+            underlying Lambda function, each sends a distinct colored payload
+            when toggled, each represented by a distinct color. On the opposite
+            side, a singular polling function extracts this data, adhering to
+            the order of the sent batches. For instance, if blue payloads are
+            sent in multiple batches followed by different colors, polling would
+            animate these blue batches sequentially, preserving their order, and
+            then progress through the subsequent colors. This visual
+            representation not only highlights the precision of data streaming
+            but also emphasizes the importance of payload sequencing.
+          </p>
+          <p className="mt-6">
+            Beyond just showcasing functionality, the project emphasizes the
+            intricate choreography between different AWS services. By leveraging
+            Lambda in tandem with Kinesis, it accentuates the synergy possible
+            in AWS{"'"}s ecosystem, underpinning the foundation of real-time
+            data handling.
+          </p>
+        </div>
+        <div
+          className="max-w-[90vw] mt-16 p-4 shadow-lg shadow-black/30 cursor-pointer bg-white rounded-xl"
+          onClick={() => window.open("/architecture.svg")}
+        >
+          <img src="/architecture.svg" alt="architecture.svg" />
+        </div>
+        <div className="mt-16 max-w-[90vw]">
+          <p>
+            The architectural SVG presents the blueprint of the entire
+            operation. Originating from a GitHub repository, it illustrates the
+            workflow: cloning the SAM project, uploading it to a personal
+            CodeCommit repository, initializing an S3 bucket for the React app,
+            setting up an IAM role for CodeBuild, and establishing the CodeBuild
+            project. With the inclusion of a buildspec within the repository, it
+            facilitates the streamlined deployment of the React application and
+            the AWS resources.
+          </p>
+          <p className="mt-6">
+            The culmination of this setup sees the deployment of the API Gateway
+            and the twin Lambda functions, each optimized for a specific task,
+            working in conjunction with a Kinesis data stream. Additionally,
+            DynamoDB plays a pivotal role, particularly in the polling function,
+            ensuring consistent data tracking and retrieval. This entire
+            endeavor underscores the possibilities of AWS services when combined
+            efficiently and effectively. Kinesis Stream: CI/CD Edition
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <footer>
+        <div className="flex flex-col items-center justify-center mt-12 md:mt-20 bg-black/10 w-full backdrop-blur h-28">
+          <p className="text-xl font-bold mt-6 mb-2">Made by Matthew Sweeney</p>
+          <div className="flex flex-row space-x-4 mb-4">
+            <a
+              href="
+                https://www.linkedin.com/in/drmatt13/"
+              className="text-blue-600 underline cursor-pointer hover:text-blue-700 visited:text-purple-600"
+            >
+              LinkedIn
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
